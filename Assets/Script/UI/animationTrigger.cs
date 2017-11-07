3321d434d15a21adf915578d29f5f03b198abb26
dmj0shua1 <dmj0shua1@gmail.com>
2017-11-07 16:45:34 +0800

    StageTransition partial

---
+++ Assets/Script/UI/animationTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animationTrigger : MonoBehaviour {
+
+    public GameObject objAnim;
+    Animator anim;
+    public string triggerName;
+
+	// Use this for initialization
+	void Start () {
+        anim = objAnim.GetComponent<Animator>();
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void setTrigger()
+    {
+        anim.SetTrigger(triggerName);       
+    }
+}
