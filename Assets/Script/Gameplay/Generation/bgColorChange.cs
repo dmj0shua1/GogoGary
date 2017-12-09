b07905d300513003b8e92afe69e55e3336c76d77
dmj0shua1 <dmj0shua1@gmail.com>
2017-12-09 14:52:48 +0800

    Background color change

---
+++ Assets/Script/Gameplay/Generation/bgColorChange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bgColorChange : MonoBehaviour {
+
+    public SpriteRenderer sprRenderer;
+
+	// Use this for initialization
+	void Start () {
+      //  sprRenderer = sprRenderer.gameObject.GetComponent<SpriteRenderer>();
+        
+   
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      //  sprRenderer.color = new Color(0f, 0f, 0f, 1f);
+	}
+}
