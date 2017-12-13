5dc84f291410819243e7de848792459330f13a27
dmj0shua1 <dmj0shua1@gmail.com>
2017-12-13 16:21:30 +0800

    Revert "Revert "Partial energy system""
    This reverts commit 347b0bf330b24101727694c392c937fc5b26e628.

---
+++ Assets/Script/UI/EnergyManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyManager : MonoBehaviour {
+
+	// Use this for initialization
+    public int energyLeft;
+    [SerializeField]
+    GameObject[] energyDrinks;
+    [Space]
+    [SerializeField]
+    Sprite energySpriteColored;
+    [SerializeField]
+    Sprite energySpriteFaded;
+	void Start () {
+        if (!PlayerPrefs.HasKey("energyLeft")) PlayerPrefs.SetInt("energyLeft", 5);
+        energyLeft = PlayerPrefs.GetInt("energyLeft");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void decreaseEnergy()
+    {
+        energyLeft--;
+        energyDrinks[energyLeft].GetComponent<SpriteRenderer>().sprite = energySpriteFaded;
+    }
+}
