c4688cc79eeb43cb4c437954f7cbaf29e8db1ca8
dmj0shua1 <dmj0shua1@gmail.com>
2017-12-20 09:02:29 +0800

    endless with star na beybe :star:

---
+++ Assets/Script/Time/SetTimeScale.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetTimeScale : MonoBehaviour {
+
+    [SerializeField]
+    float timeScale;
+
+	// Use this for initialization
+	void Start () {
+        Time.timeScale = timeScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
