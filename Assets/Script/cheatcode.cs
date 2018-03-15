8ab4c19a29d16a1a8f7ddd07d85c85ba23a37500
dmj0shua1 <dmj0shua1@gmail.com>
2018-03-15 16:58:44 +0800

    1503 version 1 update

--- Assets/Script/cheatcode.cs
+++ Assets/Script/cheatcode.cs
@@ -11,6 +11,25 @@ public class cheatcode : MonoBehaviour {
         PlayerPrefs.SetInt("TotalRescuePoints", 75);
         PlayerPrefs.SetInt("pyTotalRescuePoints", 75);
         PlayerPrefs.SetInt("phTotalRescuePoints", 75);
+        PlayerPrefs.SetInt("ieTotalRescuePoints", 75);
+
+        for (int num = 1; num < 26; num++)
+        {
+            PlayerPrefs.SetInt("Building_L" + num,3);
+        }
+        for (int num = 26; num < 51; num++)
+        {
+            PlayerPrefs.SetInt("pyBuilding_L" + num, 3);
+        }
+        for (int num = 51; num < 76; num++)
+        {
+            PlayerPrefs.SetInt("phBuilding_L" + num, 3);
+        }
+
+        for (int num = 76; num < 101; num++)
+        {
+            PlayerPrefs.SetInt("ieBuilding_L" + num, 3);
+        }
     }
     public void UnlockLevelEn()
     {
