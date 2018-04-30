cf00ccab13184780cabb4faedc4f1d03ce2d6240
dmj0shua1 <dmj0shua1@gmail.com>
2018-04-30 15:58:37 +0800

    30-04-18 version 2 gate update

---
+++ Assets/Script/Futuristic_script/ftMainGate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ftMainGate : MonoBehaviour {
+
+    [SerializeField]
+    private Animator MyAnimationGate1;
+    [SerializeField]
+    private Animator MyAnimationGate2;
+    public bool isGate;
+
+    void Start() 
+    {
+        MyAnimationGate1 = GameObject.Find("gate1").GetComponent<Animator>();
+        MyAnimationGate2 = GameObject.Find("gate2").GetComponent<Animator>();
+    }
+
+    void Update() 
+    {
+        if (isGate)
+        {
+            MyAnimationGate1.SetBool("isGate1", true);
+            MyAnimationGate2.SetBool("isGate2", false);
+        }
+        else
+        {
+            MyAnimationGate1.SetBool("isGate1", false);
+            MyAnimationGate2.SetBool("isGate2", true);
+        }
+    }
+}
