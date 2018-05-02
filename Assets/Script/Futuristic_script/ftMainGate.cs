5e1847be21ef7f5b0617a9c68def5e5180377439
dmj0shua1 <dmj0shua1@gmail.com>
2018-05-02 16:56:53 +0800

    02-05-18 version2 update

--- Assets/Script/Futuristic_script/ftMainGate.cs
+++ Assets/Script/Futuristic_script/ftMainGate.cs
@@ -7,10 +7,10 @@ public class ftMainGate : MonoBehaviour {
     [SerializeField]
     private Animator MyAnimationGate1;
     public bool isGate;
-
     void Start() 
     {
         MyAnimationGate1 = GameObject.Find("ftGateMain").GetComponent<Animator>();
+      
     }
 
     void Update() 
@@ -26,5 +26,33 @@ public class ftMainGate : MonoBehaviour {
             MyAnimationGate1.SetBool("isRightOpen", true);
 
         }
+        if (isGate)
+        {
+            StartCoroutine(changegate2());
+        }
+        else if (!isGate)
+        {
+            StartCoroutine(changegate1());
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+         if (other.gameObject.CompareTag("HitBoxCam"))
+        {
+           gameObject.SetActive(false);
+        }  
+    }
+
+    IEnumerator changegate1()
+    {
+        yield return new WaitForSeconds(2);
+        isGate = true;
+    }
+
+    IEnumerator changegate2()
+    {
+        yield return new WaitForSeconds(2);
+        isGate = false;
     }
 }
